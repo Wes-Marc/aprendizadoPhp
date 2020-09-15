b5e33a2bed7be3833087079ac5e99f1ff19ba4b7
Wes-Marc <weslei047@gmail.com>
2020-09-15 14:08:04 -0300

    Conceito persistencia - Table Data Gateway - terminado

---
+++ conceitos dos capitulos/capitulo5/Persistencia/exemplo_tdg.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'classes/tdg/ProdutoGateway.php';
+
+$data = new stdClass;
+$data->descricao = 'Vinho Brasileiro Tinto Merlot';
+$data->estoque = 8;
+$data->preco_custo = 12;
+$data->preco_venda = 18;
+$data->codigo_barras = '1352453234234';
+$data->data_cadastro = date('Y-m-d');
+$data->origem = 'N';
+
+try {
+  $conn = new PDO('mysql:host=127.0.0.1;port=3306;dbname=estoque', 'root', '');
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  ProdutoGateway::setConnection($conn);
+
+  $gw = new ProdutoGateway();
+  $gw->save($data);
+
+  $produto = $gw->find(1);
+  $produto->estoque += 2;
+  $gw->save($produto);
+  foreach ($gw->all("estoque <= 10") as $produto) {
+    print $produto->descricao . "<br>";
+  }
+}
+catch (\Exception $e) {
+  print $e->getMessage();
+}
